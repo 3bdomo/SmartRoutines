287e868210c515fde3848d677bb55e0840b92a68
HagerGamal <hagergamalty@gmail.com>
2026-04-09 00:44:29 +0200

    DashboardStatCardsRoutinCards

---
+++ SmartRoutines.UI/Controls/UC_StatCard.Designer.cs
@@ -0,0 +1,92 @@
+namespace SmartRoutines.UI.Controls
+{
+    partial class UC_StatCard
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.pnlCard = new Guna.UI2.WinForms.Guna2Panel();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblValue = new System.Windows.Forms.Label();
+            this.pnlCard.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlCard
+            // 
+            this.pnlCard.Controls.Add(this.pbIcon);
+            this.pnlCard.Controls.Add(this.lblValue);
+            this.pnlCard.Controls.Add(this.lblTitle);
+            this.pnlCard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlCard.Location = new System.Drawing.Point(0, 0);
+            this.pnlCard.Name = "pnlCard";
+            this.pnlCard.Size = new System.Drawing.Size(200, 110);
+            this.pnlCard.TabIndex = 0;
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.BackColor = System.Drawing.Color.Transparent;
+            this.pbIcon.Location = new System.Drawing.Point(150, 16);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(32, 32);
+            this.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIcon.TabIndex = 2;
+            this.pbIcon.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(65, 15);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Stat Title";
+            // 
+            // lblValue
+            // 
+            this.lblValue.AutoSize = true;
+            this.lblValue.BackColor = System.Drawing.Color.Transparent;
+            this.lblValue.Location = new System.Drawing.Point(18, 55);
+            this.lblValue.Name = "lblValue";
+            this.lblValue.Size = new System.Drawing.Size(25, 15);
+            this.lblValue.TabIndex = 1;
+            this.lblValue.Text = "0";
+            // 
+            // UC_StatCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pnlCard);
+            this.Name = "UC_StatCard";
+            this.Size = new System.Drawing.Size(200, 110);
+            this.Margin = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.pnlCard.ResumeLayout(false);
+            this.pnlCard.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel pnlCard;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblValue;
+        private System.Windows.Forms.PictureBox pbIcon;
+    }
+}
