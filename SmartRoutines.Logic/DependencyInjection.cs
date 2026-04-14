b37069c5303320f88c30a1d016ba3a72ea94e7c1
Abdelrahman Mohamed <abdulrahmanmo124@gmail.com>
2026-04-15 01:21:03 +0200

    Add Dependency Injection for logic services and refactor LiveLogger methods

---
+++ SmartRoutines.Logic/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SmartRoutines.Core.Interfaces.Logic;
+using SmartRoutines.Logic.ActionExecutors;
+using SmartRoutines.Logic.Services;
+
+namespace SmartRoutines.Logic;
+
+/// <summary>
+/// Registers logic-layer services into the application's dependency injection container.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Adds SmartRoutines logic services and action executors.
+    /// </summary>
+    /// <param name="services">The service collection to configure.</param>
+    /// <returns>The configured service collection.</returns>
+    public static IServiceCollection AddLogicServices(this IServiceCollection services)
+    {
+        if (services is null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        services.AddScoped<ILiveLogger, LiveLogger>();
+
+        services.AddScoped<IAction, LaunchAppExecutor>();
+        services.AddScoped<IAction, OpenUrlExecutor>();
+        services.AddScoped<IAction, AudioExecutor>();
+        services.AddScoped<IAction, ProcessKillerExecutor>();
+        services.AddScoped<IAction, RunCommandExecutor>();
+
+        services.AddScoped<ActionRunner>();
+        return services;
+    }
+}
+
+
